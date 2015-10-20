6b8999a9693d9e0517ffa5bead94a3795c55861d
ewgenius <ewgeniux@gmail.com>
2015-10-20 23:55:58 +0300

    material ui

--- src/app/TestComponent.tsx
+++ src/app/TestComponent.tsx
@@ -1,5 +1,10 @@
 /// <reference path="../../typings/react/react.d.ts"/>
+/// <reference path="../../typings/react-dom/react-dom.d.ts"/>
+/// <reference path="../../typings/material-ui/material-ui.d.ts"/>
+
 import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { RaisedButton } from 'material-ui';
 
 interface ITest {
   text: string;
@@ -26,6 +31,10 @@ export class TestComponent extends React.Component<ITestProps, ITestState> {
   }
 
   render() {
-    return (<div> test </div>);
+    return (
+      <div>
+        <RaisedButton label="test button" primary={true} />
+      </div>
+    );
   }
 }
--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,7 +1,9 @@
 /// <reference path="../../typings/react/react.d.ts"/>
+/// <reference path="../../typings/react-dom/react-dom.d.ts"/>
 
 import * as React from 'react';
-import { DOM } from 'react/addons';
+import * as ReactDOM from 'react-dom';
+
 import { TestComponent } from './TestComponent';
 
-React.render(React.createElement(TestComponent), document.getElementById('main'));
+ReactDOM.render(React.createElement(TestComponent), document.getElementById('main'));
